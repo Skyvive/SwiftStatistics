34378bc004115007dae669a48c193c2d7501eefe
bradhilton <brad.hilton.nw@gmail.com>
2015-05-19 20:30:39 -0600

    Initial Commit

---
+++ SwiftStatistics/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftStatistics
+//
+//  Created by Bradley Hilton on 5/19/15.
+//  Copyright (c) 2015 Skyvive. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
